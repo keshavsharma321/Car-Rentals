79094e4ca053627c679f8a04951f0f0371ca097e
keshavsharma321 <keshavsharma5676@gmail.com>
2025-04-07 00:30:17 +0530

    Description chnages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Simar tour and travels',
-  description: 'Created with v0',
-  generator: 'v0.dev',
 }
 
 export default function RootLayout({
